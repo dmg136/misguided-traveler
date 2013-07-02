7d988737b6ded9bd7b0167b5eea6e2f461edeea8
Ubuntu <ubuntu@ip-172-31-28-111.us-west-2.compute.internal>
2013-07-02 09:46:56 +0000

    try to print 1-10 on screen using function

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+var countTo10 = function() {
+  var arr = [];
+  var i = 1;
+  for (i = 1; i <= 10; i++) { arr.push(i) }
+  return arr; 
+};
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Welcome to my humble abode');
+  var arr = countTo10();
+  response.send(arr.join(" "));
 });
 
 var port = process.env.PORT || 5000;
